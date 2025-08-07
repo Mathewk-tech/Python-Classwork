f5ea6774f895342446ac224acc7bf5eedb8bb742
Mathew <mathewkariuki2005@gmail.com>
2025-08-07 11:07:56 +0300

    created var adam

--- task.py
+++ task.py
@@ -1,12 +1,27 @@
 class Human():
     def __init__(self,gender,name):
         print("The initializer was called")
-        print(name)
-        print(gender)
+        self.gender=gender
+        self.name=name
+        
+        if self.gender=="male":
+          self.ribs=23
+        else:
+             self.ribs=24
+       
 
-    def another_one(self):
+    def another_one(self,gender,name):
             print("This is another method")
+            self.gender=gender
+            self.name=name
 
 
 adam=Human(name="Mathew",gender="male")
-adam.another_one()
+print("name",adam.name)
+print("gender",adam.gender)
+print("ribs",adam.ribs)
+
+adam.another_one(name="Grace",gender="female")
+print("name",adam.name)
+print("gender",adam.gender)
+print("ribs",adam.ribs)
